5b5dd32b26d7a0cbba8632e033f9ab1aa4e67135
metaron23 <hungmetaron2@gmail.com>
2022-11-01 08:02:12 +0700

    app.css

--- resources/views/client/share/head.blade.php
+++ resources/views/client/share/head.blade.php
@@ -11,12 +11,12 @@
 <!-- Stylesheets -->
 <link rel="stylesheet" href="/assets_client/css/bootstrap.min.css">
 <link rel="stylesheet" href="/assets_client/css/plugins.css">
-<link rel="stylesheet" href="/assets_client/style.css">
 <link rel="stylesheet" href="/css/app.css">
+<link rel="stylesheet" href="/assets_client/style.css">
 
 
 <!-- Cusom css -->
-<link rel="stylesheet" href="/assets_client/css/custom.css">
+{{-- <link rel="stylesheet" href="/assets_client/css/custom.css"> --}}
 
 <!-- Modernizer js -->
 <script src="/assets_client/js/vendor/modernizr-3.5.0.min.js"></script>
@@ -28,31 +28,3 @@
 
 <script src="https://cdn.jsdelivr.net/npm/vue@2.6.10/dist/vue.js"></script>
 <script type="text/javascript" src="https://unpkg.com/default-passive-events"></script>
-<style>
-    .cancel_order a {
-        float: right;
-        transform: translateY(-96px);
-        margin-right: 10px;
-        color: red;
-        font-size: 24px;
-        width: 20px;
-        height: 20px;
-    }
-
-    .modal-mask {
-        position: fixed;
-        z-index: 9998;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        background-color: rgba(0, 0, 0, .5);
-        display: table;
-        transition: opacity .3s ease;
-    }
-
-    .modal-wrapper {
-        display: table-cell;
-        vertical-align: middle;
-    }
-</style>
--- resources/views/client/share/master.blade.php
+++ resources/views/client/share/master.blade.php
@@ -3,20 +3,6 @@
 
 <head>
     @include('client.share.head')
-    <style>
-        #load {
-            width: 100%;
-            height: 100%;
-            position: fixed;
-            z-index: 9999;
-            background: url("/assets_client/images/load.gif") no-repeat center center rgba(0, 0, 0, 0.25);
-            background-size: 4%;
-        }
-
-        #contents {
-            visibility: hidden;
-        }
-    </style>
 </head>
 
 <body>
