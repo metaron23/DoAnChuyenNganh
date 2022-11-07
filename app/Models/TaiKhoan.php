fdd7857331aca6a9a88fe7915ade49ff1be47052
metaron23 <hungmetaron2@gmail.com>
2022-11-07 08:39:34 +0700

    8:39

--- app/Models/TaiKhoan.php
+++ app/Models/TaiKhoan.php
@@ -5,12 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class TaiKhoan extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory;
 
     protected $table = "tai_khoans";
 
